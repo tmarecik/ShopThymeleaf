43b5744fa9b69e9dedbbfd7d4aff81a5224bacdb
tmarecik <tmarecik@wszib.edu.pl>
2020-02-29 17:28:13 +0100

    po przerwie

---
+++ src/main/java/pl/edu/wszib/dao/UserDao.java
@@ -0,0 +1,17 @@
+package pl.edu.wszib.dao;
+
+
+import pl.edu.wszib.domain.User;
+
+import java.util.List;
+
+
+public interface UserDao {
+    List<User> getUser();
+    void saveUser(User user);
+
+    void removeUser(Long id);
+
+    User getById(Long id);
+
+}
---
+++ src/main/java/pl/edu/wszib/dao/impl/UserDaoImpl.java
@@ -0,0 +1,47 @@
+package pl.edu.wszib.dao.impl;
+
+import org.springframework.stereotype.Repository;
+import pl.edu.wszib.dao.UserDao;
+import pl.edu.wszib.domain.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    private Map<Long, User> userMap;
+    private static long id=1L;
+
+    public UserDaoImpl() {
+        this.userMap = new HashMap<>();
+
+    }
+
+    @Override
+    public List<User> getUser() {
+        return new ArrayList<>(userMap.values());
+    }
+
+
+    @Override
+    public void saveUser(User user) {
+        if(user.getId()<1){
+            user.setId(id);
+            id++;
+        }
+    }
+
+
+    @Override
+    public void removeUser(Long id) {
+        userMap.remove(id);
+    }
+
+    @Override
+    public User getById(Long id) {
+        return userMap.get(id);
+    }
+}
